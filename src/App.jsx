51812ed54a2de694c1887b82d25f55c48c07a5cd
Azie Melza Pratama <aziemp66@gmail.com>
2022-04-04 23:31:16 +0700

    React Update Component Behind The Scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
-
-import './App.css';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button.jsx";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+	const [showParagraphs, setShowParagraphs] = useState(false);
+
+	console.log("APP RUNNING");
+
+	const tooglePararagphs = () => {
+		setShowParagraphs((prevShowParagraphs) => !prevShowParagraphs);
+	};
+
+	return (
+		<div className="app">
+			<h1>Hi there!</h1>
+			{showParagraphs && <p>This is a paragraph</p>}
+			<Button onClick={tooglePararagphs}>Click me!</Button>
+		</div>
+	);
 }
 
 export default App;
